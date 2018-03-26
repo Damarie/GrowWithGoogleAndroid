f75c3ec8f62c0c1d0a5d7ebfa442446d1cf6310d
Damarie <damarie@damarie.net>
2018-03-26 09:04:34 -0400

    End of Lesson Object Oriented Programming: Part 2

--- JustJava/app/src/main/java/com/damarie/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/damarie/justjava/MainActivity.java
@@ -1,9 +1,14 @@
 package com.damarie.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -12,7 +17,7 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 2;
+    private int quantity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,38 +29,83 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Total: $" + (quantity * 5) + "\nThank you!";
-        displayMessage(priceMessage);
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        CheckBox hasWhippedCream = findViewById(R.id.whipped_cream);
+        CheckBox hasChocolate = findViewById(R.id.chocolate);
+        EditText userName = findViewById(R.id.user_name);
+        String usernameText = userName.getText().toString();
+        int price = calculatePrice();
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT,getString(R.string.coffee_email_subject));
+        String orderSummary = createOrderSummary(usernameText, price, hasWhippedCream.isChecked(), hasChocolate.isChecked());
+
+        emailIntent.putExtra(Intent.EXTRA_TEXT, orderSummary);
+        if (emailIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(emailIntent);
+        }
+    }
+
+    /**
+     * Create summary of order
+     *
+     * @param price calculated total cost
+     * @param hasWhippedCream value of whipped cream checkbox
+     * @param hasChocolate value of chocolate checkbox
+     * @return text summary
+     */
+    private String createOrderSummary(String usernameText, int price, boolean hasWhippedCream, boolean hasChocolate) {
+        String orderSummary = getString(R.string.order_summary_name, usernameText);
+        orderSummary += "\n" + getString(R.string.add_whipped_cream, hasWhippedCream);
+        orderSummary += "\n" + getString(R.string.add_chocolate, hasChocolate);
+        orderSummary += "\n" + getString(R.string.quantity_label, quantity);
+        orderSummary += "\n" + getString(R.string.total_cost) + NumberFormat.getCurrencyInstance().format(price);
+        orderSummary += "\n" + getString(R.string.thank_you);
+        return orderSummary;
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+    private void displayQuantity(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    /**
+     * Calculates the price of the order based on the current quantity.
+     *
+     * @return the price
+     */
+    private int calculatePrice() {
+        int price = quantity * 5;
+        CheckBox whippedCream = findViewById(R.id.whipped_cream);
+        CheckBox chocolate = findViewById(R.id.chocolate);
+        if (whippedCream.isChecked()) {
+            price = price + quantity;
+        }
+        if (chocolate.isChecked()) {
+            price = price + (quantity*2);
+        }
+        return price;
     }
 
     public void increment(View view) {
-        quantity = quantity + 1;
-        display(quantity);
+        if (quantity >= 100) {
+            Toast.makeText(this, getString(R.string.too_many_alert), Toast.LENGTH_SHORT).show();
+        } else {
+            quantity += 1;
+            displayQuantity(quantity);
+        }
+
     }
 
     public void decrement(View view) {
-        quantity = quantity - 1;
-        display(quantity);
-    }
-
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        if (quantity <= 1) {
+            Toast.makeText(this, getString(R.string.too_few_alert), Toast.LENGTH_SHORT).show();
+        } else {
+            quantity = quantity - 1;
+            displayQuantity(quantity);
+        }
     }
 }
